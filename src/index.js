8d20fbd78aadf7d7961f79c2968a996e502ffd62
Eric Sanchez <bluemojo82@gmail.com>
2020-04-07 19:57:01 -0400

    initial setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+console.log("hello world")
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
